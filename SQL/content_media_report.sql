9d7eed4e51e714ff41f09c05ceded02be81db130
Nazneen Jeevunjee <98028217+NazJee@users.noreply.github.com>
2025-08-18 15:10:53 +0100

    updated make file

--- SQL/content_media_report.sql
+++ SQL/content_media_report.sql
@@ -10,7 +10,7 @@ select date_trunc('week', date)::DATE as wc_monday,
        at_product,
        sum(ctc) as spend
 from marketing_insights.bbc_mna_oc_client_database
-where wc_monday = /*'2025-07-21'*/ '<params.run_date>'
+where wc_monday = '2025-07-21' /*'<params.run_date>'*/
   and at_product = 'iplayer'
 group by 1, 2, 3
 having sum(ctc) > 100; --change to the week macro in MAP
@@ -35,8 +35,8 @@ with step_1 as (SELECT date_trunc('week', dt::date) as wc_monday,
                        count(audience_id) as impressions
                 FROM s3_audience.audience_activity
                 WHERE source = 'Custom'
-                  AND wc_monday = /*'20250721'*/
-                    to_char(('<params.run_date>')::DATE, 'YYYYMMDD')
+                  AND wc_monday = to_char(('20250721'::DATE), 'YYYYMMDD')
+                    /*to_char(('<params.run_date>')::DATE, 'YYYYMMDD')*/
                   AND app_type = 'responsive'
                   AND event_action = 'view'
                   AND (item_link LIKE '%%xtor=CS8-1000%%'
@@ -82,7 +82,7 @@ select date_trunc('week', date) as wc_monday,
        accutics_brand_id        as at_brand,
        sum(tvr_adults_16_plus)  as tvrs
 from marketing_insights.in_tv_enriched
-where wc_monday = '<params.run_date>'
+where wc_monday = /*'<params.run_date>'*/ '2025-07-21'
   and accutics_product_promoted = 'iplayer'
 group by 1, 2, 3;
 
@@ -167,11 +167,11 @@ with get_titles as (select distinct case
      get_norm_media as (select wc_date,
                                a.at_brand,
                                b.title,
-                               sum(digital_spend)    as digital_spend,
-                               sum(impressions)      as impressions,
-                               sum(tvrs)             as tvrs,
+                               sum(digital_spend)      as digital_spend,
+                               sum(impressions)        as impressions,
+                               sum(tvrs)               as tvrs,
                                sum(average_total_norm) as average_norm_media
- from norm_media a
+                        from norm_media a
                                  left join get_titles b
                                            on a.at_brand = b.pid
                         where (title != '' or title is not null)
@@ -193,11 +193,15 @@ select wc_date,
            end as media_label
 from get_percentile
 where wc_date BETWEEN
-    date_trunc('week', '<params.run_date>'::date) - interval '14 weeks'
-    AND date_trunc('week', '<params.run_date>'::date)
+          /*date_trunc('week', '<params.run_date>'::date) - interval '14 weeks'
+          AND date_trunc('week', '<params.run_date>'::date)*/
+          date_trunc('week', '2025-07-21'::date) - interval '14 weeks'
+          AND date_trunc('week', '2025-07-21'::date)
 order by wc_date desc, average_norm_media desc;
 
 
+select *
+from media_enriched;
 ------------------------------------------------------------------------------------------------------------------------
 --- Unload Report to S3 ------------------------------------------------------------------------------------------------
 ------------------------------------------------------------------------------------------------------------------------
