22248edabf1793a6ede459aae3517802eed3502c
NazJee <nazneen.jeevunjee@bbc.co.uk>
2025-08-18 14:06:05 +0000

    updated

--- ccog-content-media-report/R/ccog-content-media-report.R
+++ ccog-content-media-report/R/ccog-content-media-report.R
@@ -1,23 +1,43 @@
 #' produces a weekly report comprising media spends by title for the content team
-#'
-#' @docType package
-#' @name ccog-content-media-report
-NULL
 
-#
 # Write functions only and document them with roxygen-styled comments.
 # Example below taken from http://r-pkgs.had.co.nz/man.html
-#
 
-#' Add together two numbers.
-#' 
-#' @param x A number.
-#' @param y A number.
-#' @return The sum of \code{x} and \code{y}.
-#' @examples
-#' add(1, 1)
-#' add(10, 1)
-#' @export
-add <- function(x, y) {
-      x + y
+install.packages("tidyverse")
+library(tidyr)
+library(readxl)
+library(dplyr)
+library(RJDBC)
+library(stringr)
+library(magrittr) 
+library(data.table)
+library(lubridate)
+library(jsonlite)
+library(httr)
+library(tibble)
+library(readr)
+library(aws.s3)
+library(aws.ec2metadata)
+
+#Get Data from Redshift Table ----
+
+######### Get Redshift creds #########
+get_redshift_connection <- function() {
+  driver <-
+    JDBC(
+      driverClass = "com.amazon.redshift.jdbc.Driver",
+      classPath = "/usr/lib/drivers/RedshiftJDBC42-no-awssdk-1.2.41.1065.jar",
+      # classPath = list.files('/usr/lib/drivers/', pattern = '*.jar', full.names = TRUE), #to run in MAP locally
+      identifier.quote = "`"
+    )
+  url <-
+    str_glue(
+      "jdbc:redshift://live-idl-prod-redshift-component-redshiftcluster-1q6vyltqf8lth.ctm1v7db0ubd.eu-west-1.redshift.amazonaws.com:5439/redshiftdb?user={Sys.getenv('REDSHIFT_USERNAME')}&password={Sys.getenv('REDSHIFT_PASSWORD')}"
+    )
+  conn <- dbConnect(driver, url)
+  return(conn)
 }
+
+# Variable to hold the connection info
+conn <- get_redshift_connection()
+dbGetQuery(conn, "SELECT * FROM prez.id_profile LIMIT 10;") #check connection works
